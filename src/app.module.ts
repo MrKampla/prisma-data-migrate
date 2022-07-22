27ee8518b5550a5773eaf96a12882cadb79d2644
MrKampla <mrkampla@gmail.com>
2022-07-23 00:08:43 +0200

    Add CLI name and description

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
+import { Command } from 'commander';
+import { InjectCommander } from 'nest-commander';
 import { CreateModule } from './commands/create/create.module';
 import { InitModule } from './commands/init/init.module';
 
 @Module({
   imports: [InitModule, CreateModule],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  constructor(@InjectCommander() private readonly commander: Command) {}
+
+  onApplicationBootstrap() {
+    // set the name and description of the cli program
+    this.commander.name('prisma-data-migrate');
+    this.commander.description(
+      'Create and manage your data migrations with Prisma in TypeScript',
+    );
+  }
+}
